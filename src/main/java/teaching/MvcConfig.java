8bf925ea5e0c0ce06fd1421411f575ccd06af952
ValorCat <amorrell314@gmail.com>
2020-01-02 21:42:02 -0500

    Remove outdated view controller registry

--- src/main/java/teaching/MvcConfig.java
+++ src/main/java/teaching/MvcConfig.java
@@ -8,10 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/practice").setViewName("practice");
+
     }
 
 }
